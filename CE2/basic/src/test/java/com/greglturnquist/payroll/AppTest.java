3ecff81e11a1bbdce0745fc6db3f2b44e7aaf7b7
gui1251991 <1251991@isep.ipp.pt>
2026-05-12 21:41:43 +0100

    test: add minimal JUnit test for CE2 spring pipeline #23

---
+++ CE2/basic/src/test/java/com/greglturnquist/payroll/AppTest.java
@@ -0,0 +1,12 @@
+package com.greglturnquist.payroll;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
